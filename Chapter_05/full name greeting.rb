4fb24f8966275fda22d38c6332545c2e60f41df5
Chris Callahan <callahan.c@gmail.com>
2014-01-25 21:39:02 +0800

    Chapter 5 problems

---
+++ Chapter_05/full name greeting.rb
@@ -0,0 +1,7 @@
+puts 'What\'s your first name?'
+first = gets.chomp
+puts 'Middle?'
+middle = gets.chomp
+puts 'Surname?'
+last = gets.chomp
+puts 'Nice to meet you, ' + first + ' ' + middle + ' ' + last + '!'
---
+++ Chapter_05/new favorite number.rb
@@ -0,0 +1,4 @@
+puts 'Good afternoon! What\'s your favorite number?'
+num = gets.chomp
+better = num.to_i + 1
+puts num + ' is Ok, but ' + better.to_s + ' is a bigger and better favorite number ;)'
