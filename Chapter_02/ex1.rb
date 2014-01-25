11ea121d63da3145d3dc8cb56e263dc754fa9135
Chris Callahan <callahan.c@gmail.com>
2014-01-25 21:36:11 +0800

    Chapter 2 problems

---
+++ Chapter_02/ex1.rb
@@ -0,0 +1,8 @@
+puts "Hours in a year:"
+puts 24 * 365
+puts "Minutes in a decade:"
+puts 60 * 24 * 365 * 10
+puts "My age in seconds:"
+puts (60 * 60 * 24 * 365 * 25) + (60 * 60 * 24 * 262) + (60 * 60 * 11) + (60 * 30) + 10
+puts "The dear author's age:"
+puts 1025000000.0 / (60 * 60 * 24 * 365)
